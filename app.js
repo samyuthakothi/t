88bfb4639d0b60c646afd0d4ffc20683b9f84ff2
samyuthakothi <samyuthakothi5424@gmail.com>
2025-01-06 17:12:35 +0530

    Day-2 Assignment

---
+++ app.js
@@ -0,0 +1,31 @@
+let str1 = "double quotes";
+console.log(str1 , typeof(str1));
+
+let str2 = 'single quote'
+console.log(str2 , typeof(str2));
+
+let bool1 = true;
+console.log(bool , typeof(bool1));
+
+let bool2 = false;
+console.log(bool2 , typeof(bool2));
+
+let num1 = 10;
+console.log(num1 ,  typeof(num1));
+let num2 = 10.2345;
+
+let num3 = 10e3;
+console.log(num , typeof(num3));
+
+let a;
+
+
+
+
+
+
+
+
+
+
+
